b17fa3fb0d3a12bf8ff71b4e98c472a8707e268b
Abo_Tolba <122931774+AhmedElbassuony@users.noreply.github.com>
2025-09-08 10:01:12 +0300

    init

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Test() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Button size={"default"}>Click me</Button>
+    </div>
+  )
+}
